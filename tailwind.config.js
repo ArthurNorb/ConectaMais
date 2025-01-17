d96f66a3cd48735fb67d58036a9747804595ef15
ArthurNorb <arthurnorberto4@gmail.com>
2025-01-17 23:01:35 +0000

    006 - novo projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-<<<<<<< HEAD
-=======
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
->>>>>>> 3a4de4d2bddbc89265a3d3762026aeb332fff18b
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-<<<<<<< HEAD
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
-=======
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
 
->>>>>>> 3a4de4d2bddbc89265a3d3762026aeb332fff18b
     theme: {
         extend: {
             fontFamily: {
@@ -29,10 +18,6 @@ export default {
             },
         },
     },
-<<<<<<< HEAD
-    plugins: [],
-=======
 
     plugins: [forms, typography],
->>>>>>> 3a4de4d2bddbc89265a3d3762026aeb332fff18b
 };
